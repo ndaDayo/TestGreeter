532b5eba19cd73b237972ae64e22ab7ec0bdb015
ndaDayo <hayato@gmail.com>
2020-02-29 20:35:59 +0900

    最初の機能を追加

---
+++ src/Greeter.php
@@ -0,0 +1,11 @@
+<?php
+// src/Greeter.php
+namespace App\Greeter;
+
+class Greeter
+{
+    public function greet() :string
+    {
+        return 'おはようございます';
+    }
+}
---
+++ tests/GreeterTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Greeter;
+
+use App\Greeter\Greeter;
+use PHPUnit\Framework\TestCase;
+
+class GreeterTest extends TestCase
+{
+    /**
+     * @var Greeter
+     */
+    public $SUT;
+
+    /**
+     * @test
+     */
+    public function あいさつする()
+    {
+        $this->assertThat($this->SUT->greet(), $this->equalTo('おはようございます'));
+    }
+
+    protected function setUp()
+    {
+       $this->SUT = new Greeter;
+    }
+}
